2bed8250a7dd93b8dae1f8141f3c7c40d72de318
go-hl <dev@rhuanpk.net>
2024-08-28 21:01:25 -0300

    test: add new test for alnum func

--- alnum_test.go
+++ alnum_test.go
@@ -2,7 +2,15 @@ package normalize
 
 import "testing"
 
-func TestAlnum(t *testing.T) {
+func TestAlnumRhuanPatriky(t *testing.T) {
+	result := Alnum(`  r!Hu4n$ % Pa(TR!)1ky`)
+	expected := `rhu4n patr1ky`
+	if result != expected {
+		t.Errorf("result %q, expect %q", result, expected)
+	}
+}
+
+func TestAlnumHelloWorld(t *testing.T) {
 	result := Alnum(` @h3LLo !|   W0#Rld  `)
 	expected := `h3llo w0rld`
 	if result != expected {
